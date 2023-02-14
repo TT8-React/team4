67bf46a99ff27931afd1c90472afe67aa266500e
Abood-hass <60661627+Abood-hass@users.noreply.github.com>
2023-02-14 13:26:48 +0200

    uplate code

--- src/components/DetailsBox/index.jsx
+++ src/components/DetailsBox/index.jsx
@@ -4,17 +4,17 @@ import openBox from '../../assets/Images/open-box.png'
 
 export default function index({ title, imageSrc, onLoad }) {
     // const ContainerStyle = { backgroundColor: "#D9D9D9", width: "300px", padding: "23px 100px", borderRadius: "10px", display: "flex", flexDirection: "column", gap: "15px" }
-    const Img = ({ src }) => <img alt="" src={src || openBox} style={{ width: "100px" }} />
+    const Img = ({ src }) => <img alt="" src={imageSrc || openBox} style={{ width: "100px" }} />
 
     return (
-        <Box bgcolor={"#D9D9D9"} width={"300px"} borderRadius={"10px"} padding={"23px 0"} gap={"10px"}>
+        <Box bgcolor={"#D9D9D9"} width={"300px"} maxHeight={"130px"} borderRadius={"10px"} padding={"23px 0"} gap={"10px"}>
 
             {onLoad ?
                 <Box >
                     {[...new Array(4)].map(_ => <Skeleton style={{ margin: "auto" }} width={"200px"} animation="wave" />)}
                 </Box>
                 : <>
-                    <Img src={imageSrc || openBox} />
+                    <Img src={imageSrc} />
                     <Typography variant="h6" fontWeight={"800"} children={title || "SEBI report"} />
                 </>
             }
