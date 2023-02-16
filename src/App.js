ace39e6bb301e2f636f72079c3d23a31ac2fdd5d
Mahmoud-Nwiry <mamo992001@gmail.com>
2023-02-16 23:28:43 +0200

    add src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 
 import './App.css';
-import SideBar from './components/SideBar'
+import TransferList from './components/TransferList';
 
 function App() {
   return (
     <div className="App" style={{ height: "93vh" }}>
-      <SideBar currentRoute={3} Links={
-        [
-          {
-            //Acordian Route Exmaple
-            title: "link 1",
-            dropList: [
-              { title: "link 1.1", route: "1.1", },
-              { title: "link 1.2", route: "1.2", },
-            ]
-          },
-          //Normal Route Exmaple
-          { title: "link 2", route: "2" },
-          { title: "link 3", route: "3" },
-          { title: "link 4", route: "4" },
-        ]} />
+      <TransferList />
     </div>
   );
 }
--- src/components/SideBar/Logo/index.jsx
+++ src/components/SideBar/Logo/index.jsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material'
-import DashboardLogo from '../../../assets/Images/dashboard-logo.png'
+// import DashboardLogo from '../../../assets/Images/dashboard-logo.png'
 import React from 'react'
 
 export default function index() {
     return (
         <Box width="200px" >
-            <img style={{ width: "100px" }} alt="" src={DashboardLogo} />
+            {/* <img style={{ width: "100px" }} alt="" src={DashboardLogo} /> */}
         </Box>
     )
+    
 }
