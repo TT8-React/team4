a41e4de9ccf4613b95dddcaae9b0ba9d2d0b2c5c
belalQassem <118665701+belalQassem@users.noreply.github.com>
2023-02-16 21:34:58 +0200

    Revert "Side bar"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 
 import './App.css';
-import SideBar from './components/SideBar'
 
 function App() {
   return (
-    <div className="App" style={{ height: "93vh" }}>
-      <SideBar currentRoute={3} Links={
-        [
-          {
-            //Acordian Route Exmaple
-            title: "link 1",
-            dropList: [
-              { title: "link 1.1", route: "1.1", },
-              { title: "link 1.2", route: "1.2", },
-            ]
-          },
-          //Normal Route Exmaple
-          { title: "link 2", route: "2" },
-          { title: "link 3", route: "3" },
-          { title: "link 4", route: "4" },
-        ]} />
+    <div className="App">
+      
     </div>
   );
 }
