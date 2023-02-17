383d12b2c8853c2ced71531912e3b42fee62107f
bayansamra <abusamrabayan@gmail.com>
2023-02-17 17:19:47 +0200

    modify some files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import AccordionList from './components/AccordionList';
 
 function App() {
   return (
     <div className="App">
-      
+      <AccordionList/>
     </div>
   );
 }
