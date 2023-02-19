dd1fddc03afe34eaa239ca6628deed90606c7039
belalQassem <belalqassem52@gmail.com>
2023-02-19 15:03:22 +0200

    create login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Input from '../components/Input'
+import Button from '../components/button'
+import PageLink from '../components/PageLink'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <Input label='Email ID' placeholder='Enter Email ID'/>
+        <Input label='Password' placeholder='Enter password'/>
+        <Button text='LOG IN'/>
+        <PageLink/>
+    </div>
+  )
+}
+
+export default LoginPage
