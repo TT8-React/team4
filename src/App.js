b08cca24b385aa5c75ad986e1c8dbd8825d7bb19
Mahmoud-Nwiry <mamo992001@gmail.com>
2023-02-21 11:12:12 +0200

    add new pages to routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Dashboard from './pages/ChartPage'
 import CompanyDetails from './pages/CompanyDetails'
 import ReportsPage from './pages/ReportsPage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import MyReport from './pages/MyReport'
+import CustomizeReport from './pages/CustomizeReport'
+import UploadData from './pages/DataUpload'
 
 export default function App() {
 
@@ -18,11 +21,11 @@ export default function App() {
         <Route path="/Dashboard" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="CompanyDetails" element={<CompanyDetails />} />
-          <Route path="MyReports" element={<ReportsPage />} />
+          <Route path="MyReports" element={<MyReport />} />
           <Route path="GRIreport" element={<ReportsPage />} />
           <Route path="SEBIReport" element={<ReportsPage />} />
-          <Route path="Customizereport" element={<ReportsPage />} />
-          <Route path="DataUpload" element={<ReportsPage />} />
+          <Route path="Customizereport" element={<CustomizeReport />} />
+          <Route path="DataUpload" element={<UploadData />} />
           <Route path="Environment" element={<Dashboard />} />
           <Route path="Social" element={<Dashboard />} />
           <Route path="Governance" element={<Dashboard />} />
